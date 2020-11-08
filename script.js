27e384e7faac5beaa86820e5871f90c8c28c5788
Dominik Otocan <dominik.otocan1@gmail.com>
2020-11-08 12:46:06 +0100

    Added the "New book" button

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let myLibrary = [];
 const booksDiv = document.getElementById('books');
+const newBookContainer = document.getElementById('new-book-container');
+
+//makes the book container invisible
+newBookContainer.classList.toggle('invisibility');
 
 function Book(title, author, numberOfPages, status) {
     this.title = title;
@@ -15,8 +19,8 @@ function addBookToLibrary(newBook) {
 // myLibrary[0] = new Book('Moja biografija', 'Dominik Otocan', 169, 'read');
 // myLibrary[1] = new Book('Moja biografija-drugi deo', 'Dominik Otocan', 251, 'not read');
 
-const button = document.getElementById('add-book');
-button.addEventListener('click', function () {
+const addBookButton = document.getElementById('add-book-button');
+addBookButton.addEventListener('click', function () {
     //gets values and calls the functions that stores the values
     //into an array
     title = document.getElementById('title').value;
@@ -45,4 +49,15 @@ button.addEventListener('click', function () {
     let bookStatus = document.createElement('p');
     singleBookDiv.appendChild(bookStatus);
     bookStatus.textContent = status;
+
+    newBookContainer.classList.toggle('invisibility');
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('numberOfPages').value = '';
+    document.getElementById('status').value = '';
 });
+
+const newBookButton = document.getElementById('new-book-button');
+newBookButton.addEventListener('click', function(){
+    newBookContainer.classList.toggle('invisibility'); 
+})
